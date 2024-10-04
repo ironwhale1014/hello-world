7623b5d54053958a34eca3479934c25dc183ee6a
King <mac@mac.com>
2024-10-04 12:38:14 +0900

    BaseTable 필요없는 값 안보여 주기

--- src/common/entity/baseTable.ts
+++ src/common/entity/baseTable.ts
@@ -1,12 +1,16 @@
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 export abstract class BaseTable {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
